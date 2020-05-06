54e761b78e937af43ab32bd2ef1fe8ca386b719b
xdurfina <57987397+xdurfina@users.noreply.github.com>
2020-05-06 17:17:10 +0200

    Vytvorenie projektu

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jaroslav
+ * Date: 6. 5. 2020
+ * Time: 17:14
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+<h1>WEBTECH 2 ZAVERECNY PROJEKT</h1>
+<p>(C) Ďurfina, Plaštiak, Zaujec, Török</p>
+
+</body>
+</html>
