740bcff916ecbfebf1c4e7f749ea86a717738fbf
xdurfina <durfina.jaro@gmail.com>
2020-06-06 10:53:00 +0200

    Uprava index.php

--- index.php
+++ index.php
@@ -1,51 +1,45 @@
-<?php ?>
+<?php
 
+//$cmd=ltrim(shell_exec('ls'));
+
+//$fh = fopen('/octave-modely/lietadlo.txt','r');
+//echo $fh;
+
+ob_start();
+include("octave-modely/gulicka.m");
+ob_end_clean();
+
+//fclose($file);
+//$output = ltrim(shell_exec("octave --eval  'disp (pi); 1+1'"));
+
+$cmd= "octave --eval 'gulicka.m'";
+
+$output=exec($cmd,$out);
+var_dump($out);
+
+//echo"<pre> $out </pre>"
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Koncorocne zadanie</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- Load plotly.js into the DOM -->
+    <script src='https://cdn.plot.ly/plotly-latest.min.js'></script>
 </head>
+
 <body>
 
+    <div id='myDiv'><!-- Plotly chart will be drawn inside this DIV --></div>
+<script>
+    var data = [
+        {
+            x: ['giraffes', 'orangutans', 'monkeys','test'],
+            y: [20, 14, 23,44],
+            type: 'bar'
+        }
+    ];
 
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="api.php">API</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="octaveconsole.php">Octave console</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="statistika.php">Štatistika</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Simulations
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="simulacie/gulicka.php">Gulička</a>
-                    <a class="dropdown-item" href="simulacie/lietadlo.php">Lietadlo</a>
-                    <a class="dropdown-item" href="simulacie/tlmic.php">Tlmič</a>
-                    <a class="dropdown-item" href="simulacie/kyvadlo.php">Kyvadlo</a>
-                </div>
-            </li>
-        </ul>
-    </div>
-</nav>
-
-<br><h3 style="text-align: center">FINAL PROJECT - WEBTECH 2 </h3>
-<h5 style="text-align: center">code name {mura} / Ďurfina, Plaštiak, Zaujec, Török</h5>
+    Plotly.plot('myDiv', data);
+</script>
 
 
 </body>
